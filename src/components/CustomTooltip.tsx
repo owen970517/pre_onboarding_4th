51476f7514f6f31fab3e6e563d2bfd70640c6889
owen970517 <tamia123123@naver.com>
2023-09-11 18:32:21 +0900

    refactor: 문장 수정

--- src/components/CustomTooltip.tsx
+++ src/components/CustomTooltip.tsx
@@ -14,7 +14,7 @@ const CustomTooltip = ({active, payload}: CustomProps) => {
     if (active && payload && payload.length)
         return (
             <div className='tooltip'>
-                <h4 className='tooltip--id'>id :{payload[0].payload.id}</h4>
+                <h4 className='tooltip--id'>지역 : {payload[0].payload.id}</h4>
                 <div className='tooltip--value-wrapper'>
                     <p>value-area: {payload[0].payload.value_area}</p>
                     <p>value-bar: {payload[0].payload.value_bar}</p>

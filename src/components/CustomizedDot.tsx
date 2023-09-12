26d7bc2b5545da486180d8b84e92dec5e3ae6bae
owen970517 <tamia123123@naver.com>
2023-09-12 19:20:55 +0900

    feat: 지역 클릭 시 해당 지역에 대한 value_area를 보여주기 위한 컴포넌트

---
+++ src/components/CustomizedDot.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { IChart } from '../types/chart';
+
+interface DotProps {
+    cx?:number;
+    cy? : number;
+    payload? : IChart;
+    nowRegion : string;
+}
+
+const CustomizedDot = (props:DotProps) => {
+    const { cx, cy,nowRegion,payload } = props;
+    console.info(nowRegion)
+    if (payload?.id === nowRegion) 
+        return (
+            <circle 
+              cx={cx} 
+              cy={cy} 
+              r={5} 
+              fill="red" 
+            />
+        )
+    return null
+}
+
+export default CustomizedDot
